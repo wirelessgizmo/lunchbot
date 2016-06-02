7d793c08a9eff316ff6383c98bfba6f1f845548a
Daniel Churchill <dan@4mation.com.au>
2016-06-02 21:33:28 +1000

    commit

--- web/index.php
+++ web/index.php
@@ -5,6 +5,15 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+    array(
+        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+        'pdo.username' => $dbopts["user"],
+        'pdo.password' => $dbopts["pass"]
+    )
+);
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -17,9 +26,24 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$app->get('/', function () use ($app) {
+
+    die('a');
+    $app['monolog']->addDebug('logging output.');
+
+    $st = $app['pdo']->prepare('SELECT name FROM test_table');
+    $st->execute();
+
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
+
+    return $app['twig']->render('database.twig', array(
+        'names' => $names
+    ));
+
 });
 
 $app->run();
