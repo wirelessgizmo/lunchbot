98fad83c96a17fcffdd46d6318d058394d156e9c
Daniel Churchill <dan@4mation.com.au>
2016-06-04 10:27:08 +1000

    remove files

--- web/index.php
+++ web/index.php
@@ -2,10 +2,15 @@
 
 require('../vendor/autoload.php');
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
+
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
     array(
         'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
@@ -19,31 +24,19 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
 
 // Our web handlers
+$app->post('/lunchBot', function (Request $request) {
 
-$app->get('/', function () use ($app) {
+    var_dump($_POST);
 
-    die('in the pooper');
-    $app['monolog']->addDebug('logging output.');
-
-    $st = $app['pdo']->prepare('SELECT name FROM test_table');
-    $st->execute();
-
-    $names = array();
-    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-        $app['monolog']->addDebug('Row ' . $row['name']);
-        $names[] = $row;
-    }
-
-    return $app['twig']->render('database.twig', array(
-        'names' => $names
-    ));
-
+    return new Response('Thank you for your feedback!', 200);
 });
 
+
+
 $app->run();
+
+
+
+//5EVrWCHPRQTWP4y8ak4znfpr
