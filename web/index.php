22546a643c7ff07b3d9ba5b70eae51fc410e897c
Daniel Churchill <dan@4mation.com.au>
2016-06-04 14:36:56 +1000

    files

--- web/index.php
+++ web/index.php
@@ -9,10 +9,9 @@ use App\Validate;
 
 $options = array('hint' => array(
     'response_type' => 'ephemeral',
-    'text' => 'Use `/lunch` to make small lists to order food together',
-    'attachments' => array(array('text' => 'use `/lunch [place]` to create a new order for [place] or to show an existing list named [place].\n
-                                       use `/lunch [place] add [item] to add an [item] to the order [place].\n'))
-));
+    'text' => 'Use `/lunch` to make small lists to order food together\nUse `/lunch [place]` to create a new order for [place] or to show an existing list named [place].\n
+                                       use `/lunch [place] add [item] to add an [item] to the order [place].\n')
+);
 
 
 $app = new Silex\Application();
